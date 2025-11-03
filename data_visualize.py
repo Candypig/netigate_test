d7fa71f93c8509186abe9030ba5a634ce5b2d04a
Candypig <118898098+Candypig@users.noreply.github.com>
2025-11-03 18:46:13 +0800

    visualize: plot sample by day and group by month

--- data_visualize.py
+++ data_visualize.py
@@ -2,22 +2,44 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import data_preprocess
 import numpy as np
+from functools import reduce
 
 def show_month_plot():
     july_data = data_preprocess.get_data("row_data/7月_用電量.csv")
     august_data = data_preprocess.get_data("row_data/8月_用電量.csv")
+    sep_data = data_preprocess.get_data("row_data/9月_用電量.csv")
+    oct_data = data_preprocess.get_data("row_data/10月_用電量.csv")
     july_data, x, y = data_preprocess.preprocess(july_data)
     august_data, x, y = data_preprocess.preprocess(august_data)
-    mergy_show(july_data, august_data)
+    sep_data, x, y = data_preprocess.preprocess(sep_data)
+    oct_data, x, y = data_preprocess.preprocess(oct_data)
+    mergy_show([july_data, august_data, sep_data, oct_data])
 
 
-def mergy_show(df1, df2):
-    df = pd.merge(df1, df2, on='day', how='outer', suffixes=('_A', '_B')).sort_values('day')
+def mergy_show(dflist):
+    #df = pd.merge(df1, df2, on='day', how='outer', suffixes=('_A', '_B')).sort_values('day')
+    df = merge_dfs(dflist)
 
-    plt.scatter(df['day'], df['_value_A'], marker='o', label='july')
-    plt.scatter(df['day'], df['_value_B'], marker='s', label='august')
+
+    plt.scatter(df['day'], df['df1__value'], marker='o', label='july')
+    plt.scatter(df['day'], df['df2__value'], marker='o', label='august')
+    plt.scatter(df['day'], df['df3__value'], marker='o', label='sep')
+    plt.scatter(df['day'], df['df4__value'], marker='o', label='oct')
     plt.legend()
     plt.show()
 
+
+
+def merge_dfs(df_list, index_col='day', prefix='df'):
+
+    renamed_list = []
+    for i, df in enumerate(df_list):
+        new_cols = {col: f"{prefix}{i + 1}_{col}" for col in df.columns if col != index_col}
+        renamed_list.append(df.rename(columns=new_cols))
+
+    # 使用 merge 迭代合併
+    merged = reduce(lambda left, right: pd.merge(left, right, on=index_col, how='outer'), renamed_list)
+
+    return merged
 
 show_month_plot()
